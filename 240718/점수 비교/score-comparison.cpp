4b4a8e1442fed9ebbfe4391e4e2731f94278c335
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-07-18 15:06:29 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/점수 비교/score-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    if(a > c && b > d)
+        cout << 1;
+    else
+        cout << 0;
+    return 0;
+}
