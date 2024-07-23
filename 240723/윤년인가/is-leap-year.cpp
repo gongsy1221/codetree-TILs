c49173de42062b2bd9fed50e35e429eb6517ee3b
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-07-23 19:41:33 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/윤년인가/is-leap-year.cpp
+++ 240723/윤년인가/is-leap-year.cpp
@@ -6,17 +6,14 @@ int main() {
     cin >> a;
     if(a % 4 == 0)
     {
-        if(a % 100 == 0)
-            cout << "false";
-        else 
-            cout << "true";
+        cout << "true";
     }
     else
     {
-        if(a % 100 == 0)
-            cout << "false";
-        else 
+        if(a % 100 == 0 && a % 400 == 0)
             cout << "true";
+        else 
+            cout << "false";
     }
     return 0;
 }
