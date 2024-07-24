7c659d71cec270b329417801eb122e8027226a4b
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-07-24 19:46:55 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[12] = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 30, 31};
+    cin >> n;
+    cout << arr[n +1];
+    
+    return 0;
+}
