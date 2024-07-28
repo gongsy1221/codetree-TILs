d861d786866c07c1fbad6aedbbf62c2c5ae1b70f
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-07-28 17:40:07 +0900

    Title: n까지 숫자 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240728/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, i = 1;
+    cin >> a;
+    while(i <= a)
+    {
+        cout << i << ' ';
+        i++:
+    }
+    return 0;
+}
