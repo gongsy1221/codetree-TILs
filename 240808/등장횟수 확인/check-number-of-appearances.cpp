9ad8ef7c2f4e892b76ac9dd3238cda1f8966d879
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-08-08 15:12:17 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt =0, a;
+    for(int i =0; i <5; i++)
+    {
+        cin >> a;
+        if(a % 2== 0)
+            cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
