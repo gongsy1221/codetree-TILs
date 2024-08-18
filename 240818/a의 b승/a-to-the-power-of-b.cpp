d58c121b23b12a28cc161bb19399ecae509e37c9
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-08-18 14:25:43 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/a의 b승/a-to-the-power-of-b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b, result =1;
+    cin >> a >> b;
+    result = a;
+    for(int i = 1; i <b; i++)
+        result *= a;
+    cout << result;
+    return 0;
+}
