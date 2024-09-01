34a24913fdc18a2f2ec63d11ea89813ed117bc7d
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-09-01 22:48:06 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240901/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 240901/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(int i =i; i<= n; i++)
+    for(int i = 1; i<= n; i++)
     {
         for(int j = i + (i -1); j > 0; j--)
             cout << '*';
